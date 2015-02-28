b36d6767f5cf6ad400b2c585a1221ffc89bcb294
Noah Laferty <noah.lafferty@gmail.com>
2015-02-28 11:28:09 -0600

    tester

--- tester.js
+++ tester.js
@@ -1,13 +1,10 @@
 var io = require('socket.io-client')('ws://localhost:3000');
 
-console.log('here');
+io.emit('register', 'pilot');
+io.emit('action', {type:'navigation', place: 'blah'});
 
-//io.emit('register', 'pilot');
-//console.log('here');
-//io.emit('action', {type:'navigation', place: 'blah'});
-//console.log('here');
 
-io.emit('register', 'engineer');
-console.log('here');
-io.emit('action', {type:'allocate', system: 'shields'});
-console.log('here');
+//io.emit('register', 'engineer');
+//console.log('here');
+//io.emit('action', {type:'allocate', system: 'shields'});
+//console.log('here');
