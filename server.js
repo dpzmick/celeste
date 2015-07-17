151b5c455f7c6a1510876fef3d160c582f770848
dpzmick <dpzmick@gmail.com>
2015-07-16 21:58:17 -0400

    refactor server

--- server.js
+++ server.js
@@ -1,76 +1,52 @@
-var _ = require('underscore');
+var _    = require('lodash');
+var path = require('path');
 
-var pilotFactory           = require('./pilot.js');
-var engineerFactory        = require('./engineer.js');
-var modelFactory           = require('./gameModel.js');
-var alertControllerFactory = require('./alertController.js');
+var Pilot     = require('./lib/pilot.js');
+var Engineer  = require('./lib/engineer.js');
+var Model     = require('./lib/gameModel.js');
+var GameError = require('./lib/errors/game-error.js');
 
 var app  = require('express')();
 var http = require('http').Server(app);
-var io   = require('socket.io')(http)
+var io   = require('socket.io')(http);
 
-var alerter = require('./alertController.js')(io);
-var model = modelFactory(alerter);
+var alerter = require('./lib/alertController.js')(io);
+var model = new Model(alerter);
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
-
-var roleFactories = {
-    pilot:    pilotFactory,
-    engineer: engineerFactory,
+var roleConstructors = {
+    pilot:    Pilot,
+    engineer: Engineer
 };
 
+var socketData = {};
+
+var roles = {};
+
 var checkValidRole = function (role) {
-    return _.has(roleFactories, role);
-}
+    return _.has(roleConstructors, role);
+};
 
 var checkRoleTaken = function (role) {
     return _.has(roles, role);
-}
+};
 
 var checkSocketHasRole = function (socket) {
     return _.has(socketData, socket.id);
-}
+};
 
-var attemptRegisterRole = function (role, socket) {
-    if (!checkValidRole(role)) {
-        console.log('invalid role ' + role)
-        return {
-            type: 'error',
-            msg: 'invalid role',
-            role: role
-        }
-    }
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-    if (checkRoleTaken(role)) {
-        console.log('role ' + role + ' already taken');
-        return {
-            type: 'error',
-            msg: 'role taken',
-            role: role,
-        }
-    }
+io.on('connection', function (socket) {
+    console.log('user connected');
 
-    roles[role] = roleFactories[role](model);
-    alerter.registerSocketFor(role, socket)
-    socketData[socket.id] = role;
-    alerter.someoneRegistered(role);
-
-    console.log('role ' + role + ' has been claimed');
-
-    return roles[role].getInitialStateData();
-}
-
-var makeRegisterFunction = function(socket) {
-    return function(role, ackFun) {
-        var role = role;
-        ackFun(attemptRegisterRole(role, socket));
-    }
-}
-
-var makeDisconnectFunction = function (socket) {
-    return function () {
+    /**
+     * handle a disconnect.
+     *
+     * remove the socket from anything it might be registered to
+     */
+    socket.on('disconnect', function () {
         if (_.has(socketData, socket.id)) {
             var role = socketData[socket.id];
             delete socketData[socket.id];
@@ -80,39 +56,60 @@ var makeDisconnectFunction = function (socket) {
         } else {
             console.log('someone with no role disconnected');
         }
-    }
-}
+    });
 
-var socketData = {};
-var roles = {};
-io.on('connection', function (socket) {
-    console.log('user connected');
+    /**
+     * on the register even, attempt to register the connected socket using the
+     * given role.
+     *
+     * Call the acknowledge function with an error if there is one, otherwise
+     * call it with no error
+     */
+    socket.on('register', function (role, ackFun) {
+        if (!checkValidRole(role)) {
+            console.log('invalid role ' + role);
+            return ackFun(new GameError(GameError.codes.INVALID_ROLE, 'invalid role ' + role));
+        }
 
-    var myRegister = makeRegisterFunction(socket);
-    var myDisconnect = makeDisconnectFunction(socket);
+        if (checkRoleTaken(role)) {
+            console.log('role ' + role + ' already taken');
+            return ackFun(new GameError(GameError.codes.ROLE_TAKEN, 'role taken ' + role));
+        }
 
-    socket.on('disconnect', myDisconnect);
-    socket.on('register', myRegister);
+        roles[role] = new roleConstructors[role](model);
+        socketData[socket.id] = role;
 
+        alerter.registerSocketFor(role, socket);
+        alerter.someoneRegistered(role);
+
+        console.log('role ' + role + ' has been claimed');
+        ackFun(null, roles[role].getInitialStateData());
+    });
+
+    /**
+     * on the action event, handle the given action, then call the callback with
+     * the result (if there is one);
+     */
     socket.on('action', function (action, ackFun) {
         console.log('called');
+
         if (!checkSocketHasRole(socket)) {
-            var payload = {
-                type: 'error',
-                msg: 'this client has not registered',
-            };
-            ackFun(payload);
+            ackFun(new GameError(GameError.codes.NOT_REGISTER, 'client not registered'));
         } else {
             var role = socketData[socket.id];
-            roles[role].handleAction(action);
 
-            if (typeof ackFun !== 'undefined' && ackFun) {
-                ackFun(true);
-            }
+            // attempt to handle the action with the given role,
+            // if an error occurs, return that error through the ackFun
+            roles[role].handleAction(action).then(function (result) {
+                ackFun(null, result);
+            }).catch(function (error) {
+                console.log(error);
+                ackFun(error);
+            });
         }
     });
 });
 
-http.listen(process.env.PORT || 3000 , function () {
+http.listen(process.env.PORT || 3000, function () {
     console.log('listening on 3000');
-})
+});
