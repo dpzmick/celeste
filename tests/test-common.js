5a2e3bc142b8b4bbd98394d6112da6aa0202c3bb
dpzmick <dpzmick@gmail.com>
2015-07-30 20:42:15 -0400

    added role 'abstract class' thing

--- tests/test-common.js
+++ tests/test-common.js
@@ -9,7 +9,12 @@ var assert = require('chai').assert;
 var checkRoleInterface = function (obj) {
     // can't check that the constructor takes the args we need it to
     assert.equal(typeof obj.handleAction, 'function');
+    assert.notOk(obj.handleAction.IS_FAKE_STUB,
+                 'the error-generating method from Role has not been overridden');
+
     assert.equal(typeof obj.getInitialStateData, 'function');
+    assert.notOk(obj.getInitialStateData.IS_FAKE_STUB,
+                 'the error-generating method from Role has not been overridden');
 };
 
 module.exports = {
