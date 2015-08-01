2debae2773b1bc6c5f9991de3574d0bfff3402bf
dpzmick <dpzmick@gmail.com>
2015-07-31 22:45:30 -0400

    slight changes to client, still lots of work needs to go into server

--- public/js/main.js
+++ public/js/main.js
@@ -5,28 +5,26 @@ var socket = io(),
 $('.register button').click(function() {
     var role = $(this).val();
     console.log(role);
-    
-    socket.emit('register', role, function (done) {
-        console.log(done);
-        
-        if (done.type && done.type == "error") {
-            $('.error').text(role + " is already registered!");
-        } else {
-            $('.register').slideUp();
-            $('.' + role + '-console').slideDown();
 
-            myRole = role;
-            registered = true;
-
-            init(done);
+    socket.emit('register', role, function (error, initialState) {
+        if (error) {
+            return $('.error').text(error.message);
         }
+
+        $('.register').slideUp();
+        $('.' + role + '-console').slideDown();
+
+        myRole = role;
+        registered = true;
+
+        init(initialState);
     });
 });
 
-var roleUpdate = { 
+var roleUpdate = {
     pilot: function(msg) {
         switch (msg.type) {
-            case "allocation":
+            case 'allocation':
                 $('[data-power]').removeClass('powered');
                 $('[data-power="' + msg.system + '"]').addClass('powered');
                 break;
@@ -54,13 +52,17 @@ function moveShip() {
         newY = $('#ship-y').val();
 
     console.log(newX, newY);
-    socket.emit('action', 
-        { type: 'navigation', x: newX, y: newY });
+    socket.emit('action', { type: 'navigation', x: newX, y: newY }, function (err) {
+        if (err) {
+            // every action gets an ackFun. First arg is error if there was one,
+            // otherwise undefined
+            console.log(err);
+        }
+    });
 }
 
 function allocatePower(target) {
-    socket.emit('action', 
-        { type: 'allocate', system: target });
+    socket.emit('action', { type: 'allocate', system: target });
 }
 
 $('.console-wrapper').on('click', '.action', function() {
@@ -69,10 +71,10 @@ $('.console-wrapper').on('click', '.action', function() {
     console.log(action, target);
 
     switch(action) {
-        case "navigation":
+        case 'navigation':
             moveShip();
             break;
-        case "allocate":
+        case 'allocate':
             allocatePower(target);
             break;
         default:
